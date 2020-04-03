9cfcf1c8ee3afc7e7c2385a870a2b70cd72c3cac
Oleksii Polevin <pololex327@gmail.com>
2020-04-03 16:10:11 +0300

    refactor(books/service): rewrite aggregation

--- src/components/Books/service.js
+++ src/components/Books/service.js
@@ -5,21 +5,20 @@ const BooksModel = require('./model');
  * @returns {Promise < void >}  books code3's and their values
  */
 async function getChartData() {
-    const result = [];
     const books = await BooksModel.aggregate([{
         $group:
-        { _id: { code3: '$code3' }, value: { $sum: 1 } },
-    }]);
-    books.forEach((item) => {
-        const chank = {
-            // eslint-disable-next-line no-underscore-dangle
-            code3: item._id.code3,
-            value: item.value,
-        };
-        result.push(chank);
-    });
+        { _id: '$code3', value: { $sum: 1 } },
+    },
+    {
+        $project: {
+            _id: 0,
+            code3: '$_id',
+            value: 1,
+        },
+    },
+    ]);
 
-    return result;
+    return books;
 }
 
 module.exports = {
